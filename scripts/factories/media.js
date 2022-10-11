74307613811d185275bb05af8f126b8062674b95
Kilian LEROY <kilian.leroy@me.com>
2022-10-11 22:11:29 +0200

    VF
    Remplacement Image par Video

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -8,23 +8,25 @@ function MediaFactory(data) {
     function MediaCardDOM() {
         const divMedia = document.createElement('div')
         divMedia.setAttribute('class',"vignette-media")
-        const article = document.createElement( 'article' );
-        const imgMedia = document.createElement( 'img' );
-        imgMedia.setAttribute("src", picture);
-        imgMedia.setAttribute("alt", "");
+        if(image === undefined){
+            const vidMedia = document.createElement( 'video' );
+            vidMedia.setAttribute("src", vid);
+            vidMedia.setAttribute("alt", "");
+            divMedia.appendChild(vidMedia);
+        }else{
+            const imgMedia = document.createElement( 'img' );
+            imgMedia.setAttribute("src", picture);
+            imgMedia.setAttribute("alt", "");
+            divMedia.appendChild(imgMedia);
+        }
         const divMediaBot = document.createElement('div')
         divMediaBot.setAttribute('class',"vignette-media-bot")
-
-        const vidMedia = document.createElement( 'video' );
-        vidMedia.setAttribute("src", vid);
-        vidMedia.setAttribute("alt", "");
         const titleMedia = document.createElement( 'div' );
         titleMedia.setAttribute('class',"vignette-media-title")
         titleMedia.textContent = title;
         const likeMedia = document.createElement( 'div' );
         likeMedia.setAttribute('class',"vignette-media-like")
         likeMedia.textContent = likes;
-        divMedia.appendChild(imgMedia)
         divMedia.appendChild(divMediaBot);
         divMediaBot.appendChild(titleMedia);
         divMediaBot.appendChild(likeMedia);
@@ -35,37 +37,3 @@ function MediaFactory(data) {
 
 
 }
-
-//
-
-// if(picture === undefined)
-
-// function capture() {
-//     var canvas = document.getElementById("canvas");
-//     var video = document.querySelector("video");
-//     canvas.width = video.videoWidth;
-//     canvas.height = video.videoHeight;
-//     canvas
-//       .getContext("2d")
-//       .drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  
-//     /** Code to merge image **/
-//     /** For instance, if I want to merge a play image on center of existing image **/
-//     const playImage = new Image();
-//     playImage.src = "path to image asset";
-//     playImage.onload = () => {
-//       const startX = video.videoWidth / 2 - playImage.width / 2;
-//       const startY = video.videoHeight / 2 - playImage.height / 2;
-//       canvas
-//         .getContext("2d")
-//         .drawImage(playImage, startX, startY, playImage.width, playImage.height);
-//       canvas.toBlob() = (blob) => {
-//         const img = new Image();
-//         img.src = window.URL.createObjectUrl(blob);
-//       };
-//     };
-//     /** End **/
-//   }
-  
-//   capture();
-  
